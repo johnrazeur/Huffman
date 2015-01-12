4945db8ec802f5d81c035623c57876974317e5b1
johnrazeur <johnrazeur@gmail.com>
2015-01-12 14:09:22 +0100

    ajout arguments

--- main.cpp
+++ main.cpp
@@ -11,21 +11,52 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-	/*Arbre *arbre = new Arbre(NULL, 14);
-	Arbre *arbreg = new Arbre(arbre, 6);
-	Arbre *arbred = new Arbre(arbre, 17);
-	arbre->setD(arbred);
-	arbre->setG(arbreg);
-	arbre->insert_feuille(7);*/
-	//arbre->affiche_croissant();
-	//arbre->print(20);
+
+	if(argc > 1)
+	{
+		if(argv[1] == "--help")
+		{
+			cout << "Paramètres :" <<endl;
+			cout << "-c [filein] -o [fileout.hff] : Compresse le fichier [filein]" << endl;
+			cout << "-x [filein.hff] : Décompresse le fichier [fileing.hff]" << endl;
+		}
+		else if(argv[1] == "-c")
+		{
+			if(argc > 4)
+			{
+
+			}
+			else
+			{
+				cout << "Utilisation de -c :" << endl;
+				cout << "-c [filein] -o [fileout.hff] : Compresse le fichier [filein]" << endl;
+			}
+		}
+		else if(argv[1] == "-x")
+		{
+			if(argc > 2)
+			{
+
+			}
+			else
+			{
+				cout << "Utilisation de -x:" << endl;
+				cout << "-x [filein.hff] : Décompresse le fichier [fileing.hff]" << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "Pas de paramètres" << endl;
+		cout << "Faite --help pour de l'aide" << endl;
+	}
 
 	string tab = "abcdefghijklmnopqrstuvwxyz_";
 
 	Huffman *huff = new Huffman(tab);
-	
+
 	string mot = "anticonstitutionnellement";
 
 	vector<int> freq = huff->getFreq(mot);
@@ -34,16 +65,16 @@ int main()
  	vector <Arbre*> trie = huff->trier();
  	Arbre * comp = huff->compression();
  	huff->creerTab(comp, "");
-	 
+
 
  	map<char, string> tabHuffman = huff->getTabHuffman();
  	/*for(unsigned int i = 0; i < mot.length(); i++)
 	{
 		cout << tabHuffman[mot[i]];
 	}*/
-	
+
 	string motcode = "110000001101110011110001111110110101111100110111100000100110111011001111101000001";
-	
+
 	huff->decompression(motcode);
 
 }
